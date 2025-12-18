c422ec2015e397ef94bcde66d26dfbeaed9832e1
Opilane TTHK <opilane@tthk.ee>
2025-12-18 11:39:21 +0200

    wfw
    frfwf

--- UUs töö/Ülesanded/ÜL/Program.cs
+++ UUs töö/Ülesanded/ÜL/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace ÜL
+﻿using System.ComponentModel.Design;
+
+namespace ÜL
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //kirjuta programmm mis
+            //küsib kasutajalt tsüklis ,illised mängud telle meeldivad
+            //küsimuste tulemus hoia järjednis
+            List<string> mängud = new List<string>();
+            Console.WriteLine("Tere kasutaja millised mängud slle meeldivad");
+            string mäng = "";
+            while (mäng != "ei ole")
+            {
+                Console.WriteLine("palun sisesta järgmine mäng keui ei ole kirjuta \"ei ole\"");
+                mäng = Console.ReadLine();
+                if (mäng != "ei ole")
+                {
+                    mängud.Add(mäng);
+                }
+            }
+            //kui järjend sisaldab (.Contains()) "mario kart" siis küsi kui vana ta on sarkatiliselt
+            if (mängud.Contains("mario kart"))
+            {
+                Console.WriteLine("kui vana sa oled??!?mingi kuus vä?!?!");
+            }
+            //kui järjend sisaldab "gta 6" siis küsi kust ta ajamasin sai et rockstar lõpuks ajsaga valmis sai
+            if (mängud.Contains("gta 6"))
+            {
+                Console.WriteLine("kust ajamasina said? Kuidas rockstar arendusega juurde sai?");
+            }
+            //kui järjend sisaldab "pong" siis ütle "80ndad tahavad vanureid tagasi tpoimub boomerite recall
+            if (mängud.Contains ("pong"))
+            {
+                Console.WriteLine("80ndad tahavad vanureid tagasi toimbub boomerite recall");
+            }
+            //kui järjend on tühi siis programm läheb edasi ->
+
+            //küsi kasutajalt kas talle ei meeldi mängud ning löase tal vastata sõnaga jah või ei kontrolli kasutaja sisestust tOuPPER() VÕI tOlOWER()iga
+            if (!mängud.Any())
+            {
+                Console.WriteLine("Kas sulle ei meeldi mängud? vasta kas jah või ei");
+                string jahvõiei = Console.ReadLine().ToLower();
+                if (jahvõiei.Contains("jah"))
+                {
+                    Console.WriteLine("aga miks siis ei sisestanud?");
+                }
+                else if (jahvõiei.Contains("ei"))
+                {
+                    Console.WriteLine("kahju :c");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("ei saa aru :/");
+            }
+            //kui talle ei meeldi siis ütle kahju
+            //kui ta vastab jah siis ütle aga miks siis ei sisestanud?
+            //kui vastus on midagi muud siis üte ei saa aru :/
         }
     }
 }
--- UUs töö/Ülesanded/ÜL2/ÜL2/Program.cs
+++ UUs töö/Ülesanded/ÜL2/ÜL2/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //kirjuta programm kus on kaks kasutajat (kasutajanime ja parooliga)
+            //küsi kasutjalt tema kasutajanime otsi kasutajate järjendist vastav kasutaja ja kui kasutajat ei leita ütle et kasutaja ei eksisteeri
+            //küsi kasutaja ekisteerib vaata kasutaja parooli küsi kasutajalt tema parool ja võrdne neid
+            //kui paroolid ühtiad ütle tere tulemast
+            //kui aga ei ühti küsi kasutajalt parool uuesti
+
+            List<List<string>> kasutajad = new List<List<string>>();
+            kasutajad.Add(new List<string> { "kasutajanimi1", "parool1" });
+            kasutajad.Add(new List<string> { "kasutajanimi2", "parool2" });
+            //foreach (var item in kasutajad)
+            //{
+            //    Console.WriteLine(item.ToString() + "," + item[0] + "," + item[1]);
+            //}
+            string kasutajanimi = "";
+            Console.WriteLine("palun sisesta oma kasutajanimi");
+            kasutajanimi = Console.ReadLine();
+            bool doesuserexsist = false;
+            string õigeparool = "";
+            foreach (var user in kasutajad)
+            {
+                if (user[0] == kasutajanimi)
+                {
+                    doesuserexsist = true;
+                    õigeparool = user[1];
+                }
+            }
+            if (doesuserexsist == true)
+            {
+                string sisestus = "";
+                Console.WriteLine("´Tere",kasutajanimi,"sisesta oma parool");
+                while (sisestus != õigeparool)
+                {
+                    Console.WriteLine("palun sisesta oma parool:");
+                    sisestus = Console.ReadLine();
+                }
+                Console.WriteLine($"Tere tulemast {kasutajanimi}");
+
+            }
+            else
+            {
+                Console.WriteLine("kasutaja ei eksisteeri");
+            }
+        
         }
     }
 }
