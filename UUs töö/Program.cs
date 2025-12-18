466f0b6daac774022f1f6cf0bf90372ee4095e07
Opilane TTHK <opilane@tthk.ee>
2025-12-18 09:28:14 +0200

    Sõne

---
+++ UUs töö/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace UUs_töö
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string lause = "Tere  hommikust, mul on monster otsas, kas ma võin veel magada?";
+            ////Console.WriteLine("lause pikkus on: " + lause.Length);
+            ////Console.WriteLine("see tekst on capslockis:" + lause.ToUpper());
+            ////Console.WriteLine("see tekst on väiketähtedes:" + lause.ToLower());
+            ////Console.WriteLine("kas lauses on sõna \"magada\"?:" + lause.ToLower().Contains("magada"));
+
+            ////List<string> list = new List<string>() { "auto", "kraav", "kruus" };
+            ////string listkoos = "";
+            ////foreach (var sõne in list)
+            ////{
+            ////    listkoos += ("" + sõne.ToUpper() + ",");
+            ////}
+            ////Console.WriteLine(listkoos);
+
+            ////string eesnimi = "Julius";
+            ////string perekonnanimi = "Caesar";
+            ////string amet = "Ohver";
+            ////string kuuluskeiser = string.Concat(eesnimi, perekonnanimi, amet);
+            ////Console.WriteLine(kuuluskeiser);
+
+            ////string kuuluskeiser2 = $"{eesnimi},{perekonnanimi},{amet}";
+            ////Console.WriteLine($"{eesnimi},{perekonnanimi},{amet}");
+            ////Console.WriteLine(kuuluskeiser2);
+
+            ////string[] sõnadlauses = lause.Split(' ');
+            ////foreach (var sõne in sõnadlauses)
+            ////{
+            ////    Console.WriteLine(sõne);
+            ////}
+            ////string ükssõna = "kuulillennuteetunneliluuk";
+            ////Console.WriteLine(ükssõna[4]);
+            ////Console.WriteLine(ükssõna.IndexOf("i"));
+
+            //Console.WriteLine(" \" ");
+
+            //Console.WriteLine("a \nb ");
+
+            //Console.WriteLine("AAAA\tBBBB");
+        }
+    }
+}
